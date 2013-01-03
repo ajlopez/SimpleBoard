90b64047a763b7345167bd33e58e619fe261d933
ajlopez <webmaster@ajlopez.com>
2013-01-03 19:15:49 -0300

    putCell with test

---
+++ test/putget.js
@@ -0,0 +1,17 @@
+
+var simpleboard = require('../'),
+    assert = require('assert');
+    
+// putCell, getCell
+
+var board = simpleboard.createBoard();
+var cell = { color: 1 };
+
+board.putCell(0, 0, cell);
+board.putCell(1, 1, cell);
+
+assert.equal(board.getCell(0, 0), cell);
+assert.equal(board.getCell(1, 1), cell);
+assert.equal(board.getCell(0, 1), null);
+assert.equal(board.getCell(1, 0), null);
+
