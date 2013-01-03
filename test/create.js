d2ba374ce90f50108d86e28d6c4fa71c50b7d51c
ajlopez <webmaster@ajlopez.com>
2013-01-03 19:10:12 -0300

    Create rectangular board tests

--- test/create.js
+++ test/create.js
@@ -17,3 +17,25 @@ assert.equal(board.getHeight(), 8);
 for (var x = 0; x < board.getWidth(); x++)
     for (var y = 0; y < board.getHeight(); y++)
         assert.equal(board.getCell(x, y), null);
+
+// create rectangular board
+
+var board = simpleboard.createBoard(4, 8);
+assert.ok(board);
+assert.equal(board.getWidth(), 4);
+assert.equal(board.getHeight(), 8);
+
+for (var x = 0; x < board.getWidth(); x++)
+    for (var y = 0; y < board.getHeight(); y++)
+        assert.equal(board.getCell(x, y), null);
+
+// create rectangular board 2
+
+var board = simpleboard.createBoard(6, 3);
+assert.ok(board);
+assert.equal(board.getWidth(), 6);
+assert.equal(board.getHeight(), 3);
+
+for (var x = 0; x < board.getWidth(); x++)
+    for (var y = 0; y < board.getHeight(); y++)
+        assert.equal(board.getCell(x, y), null);
