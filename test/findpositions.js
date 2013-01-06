3849a3c7c52674f3a8d3afe17ac451850234f3d4
ajlopez <webmaster@ajlopez.com>
2013-01-06 08:44:25 -0300

    Additional test for findPositions

--- test/findpositions.js
+++ test/findpositions.js
@@ -33,3 +33,18 @@ assert.equal(positions.length, 1);
 assert.equal(positions[0].x, 1);
 assert.equal(positions[0].y, 1);
 
+// Add content
+
+board.putContent(0, 2, content1);
+board.putContent(1, 2, content2);
+
+var positions = board.findPositions(function(content) { return content.color === 1; });
+
+assert.ok(positions);
+assert.equal(positions.length, 2);
+
+var positions = board.findPositions(function(content) { return content.color === 2; });
+
+assert.ok(positions);
+assert.equal(positions.length, 2);
+
