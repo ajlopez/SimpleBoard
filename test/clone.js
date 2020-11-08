237a71e1b423ea5b4a00b1383a1fa59c24851640
ajlopez <webmaster@ajlopez.com>
2020-11-08 09:57:46 -0300

    Clone tests refactor

--- test/clone.js
+++ test/clone.js
@@ -1,72 +1,93 @@
 
-var simpleboard = require('../'),
-    assert = require('assert');
+const simpleboard = require('../');
+const assert = require('assert');
     
 // clone default board
 
-var original = simpleboard.createBoard();
-var board = original.clone();
-assert.ok(board);
-assert.equal(board.getWidth(), 8);
-assert.equal(board.getHeight(), 8);
+{
+    const original = simpleboard.createBoard();
+    const board = original.clone();
+    assert.ok(board);
+    assert.equal(board.getWidth(), 8);
+    assert.equal(board.getHeight(), 8);
 
-for (var x = 0; x < board.getWidth(); x++)
-    for (var y = 0; y < board.getHeight(); y++)
-        assert.equal(board.getContent(x, y), null);
+    for (let x = 0; x < board.getWidth(); x++)
+        for (let y = 0; y < board.getHeight(); y++)
+            assert.equal(board.getContent(x, y), null);
+}
 
 // create rectangular board
 
-var original = simpleboard.createBoard(4, 8);
-var board = original.clone();
-assert.ok(board);
-assert.equal(board.getWidth(), 4);
-assert.equal(board.getHeight(), 8);
+{
+    const original = simpleboard.createBoard(4, 8);
+    const board = original.clone();
+    
+    assert.ok(board);
+    assert.equal(board.getWidth(), 4);
+    assert.equal(board.getHeight(), 8);
 
-for (var x = 0; x < board.getWidth(); x++)
-    for (var y = 0; y < board.getHeight(); y++)
-        assert.equal(board.getContent(x, y), null);
+    for (let x = 0; x < board.getWidth(); x++)
+        for (let y = 0; y < board.getHeight(); y++)
+            assert.equal(board.getContent(x, y), null);
+}
 
 // create rectangular board 2
 
-var original = simpleboard.createBoard(6, 3);
-var board = original.clone();
-assert.ok(board);
-assert.equal(board.getWidth(), 6);
-assert.equal(board.getHeight(), 3);
+{
+    const original = simpleboard.createBoard(6, 3);
+    const board = original.clone();
 
-for (var x = 0; x < board.getWidth(); x++)
-    for (var y = 0; y < board.getHeight(); y++)
-        assert.equal(board.getContent(x, y), null);
+    assert.ok(board);
+    assert.equal(board.getWidth(), 6);
+    assert.equal(board.getHeight(), 3);
+
+    for (let x = 0; x < board.getWidth(); x++)
+        for (let y = 0; y < board.getHeight(); y++)
+            assert.equal(board.getContent(x, y), null);
+}
 
 // clone with content
 
-var original = simpleboard.createBoard();
-var content1 = { color: 1 };
-var content2 = { color: 2 };
+{
+    const original = simpleboard.createBoard();
+    const content1 = { color: 1 };
+    const content2 = { color: 2 };
 
-original.putContent(0, 0, content1);
-original.putContent(1, 1, content2);
+    original.putContent(0, 0, content1);
+    original.putContent(1, 1, content2);
 
-var board = original.clone();
+    const board = original.clone();
 
-var positions = board.getPositions();
+    const positions = board.getPositions();
 
-assert.ok(positions);
-assert.equal(positions.length, 2);
-assert.equal(positions[0].x, 0);
-assert.equal(positions[0].y, 0);
-assert.equal(positions[0].content, content1);
-assert.equal(positions[1].x, 1);
-assert.equal(positions[1].y, 1);
-assert.equal(positions[1].content, content2);
-assert.deepEqual(positions, original.getPositions());
+    assert.ok(positions);
+    assert.equal(positions.length, 2);
+    assert.equal(positions[0].x, 0);
+    assert.equal(positions[0].y, 0);
+    assert.equal(positions[0].content, content1);
+    assert.equal(positions[1].x, 1);
+    assert.equal(positions[1].y, 1);
+    assert.equal(positions[1].content, content2);
+    assert.deepEqual(positions, original.getPositions());
+}
 
 // then alter original board without chaning the cloned one
 
-var content3 = { color: 3 };
+{
+    const original = simpleboard.createBoard();
+    const content1 = { color: 1 };
+    const content2 = { color: 2 };
 
-original.putContent(2, 2, content3);
+    original.putContent(0, 0, content1);
+    original.putContent(1, 1, content2);
 
-assert.equal(board.getPositions().length, 2);
-assert.equal(original.getPositions().length, 3);
+    const board = original.clone();
+
+    const content3 = { color: 3 };
+
+    original.putContent(2, 2, content3);
+
+    assert.equal(board.getPositions().length, 2);
+    assert.equal(original.getPositions().length, 3);
+}
 
