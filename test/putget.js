80255296a22c9a96caab62ac385d3deb0267ac1d
ajlopez <webmaster@ajlopez.com>
2020-11-12 11:32:24 -0300

    Refactor get, put tests

--- test/putget.js
+++ test/putget.js
@@ -1,17 +1,19 @@
 
-var simpleboard = require('../'),
+const simpleboard = require('../');
     assert = require('assert');
     
 // putContent, getContent
 
-var board = simpleboard.createBoard();
-var cell = { color: 1 };
+{
+    const board = simpleboard.createBoard();
+    const cell = { color: 1 };
 
-board.putContent(0, 0, cell);
-board.putContent(1, 1, cell);
+    board.putContent(0, 0, cell);
+    board.putContent(1, 1, cell);
 
-assert.equal(board.getContent(0, 0), cell);
-assert.equal(board.getContent(1, 1), cell);
-assert.ok(board.getContent(0, 1) === null);
-assert.ok(board.getContent(1, 0) === null);
+    assert.equal(board.getContent(0, 0), cell);
+    assert.equal(board.getContent(1, 1), cell);
+    assert.ok(board.getContent(0, 1) === null);
+    assert.ok(board.getContent(1, 0) === null);
+}
 
